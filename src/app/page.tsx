99f2ad3d34d19d76db8d16dab48fa054627671ab
Aditya Sharma <adisharma4857@gmail.com>
2025-08-13 16:49:52 +0530

    fix: replaced quotes with escaped entity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <Image src={"logo.svg"} alt="CareFence logo" width={360} height={100} />
-      <p className="italic mt-4 text-xl">"Clock in within the safe zone"</p>
+      <p className="italic mt-4 text-xl">&quot;Clock in within the safe zone&quot;</p>
     </div>
   );
 }
