12a00ba0008478cd848adfe4560fe0aa09dd6d29
Aditya Sharma <adisharma4857@gmail.com>
2025-08-15 20:46:32 +0530

    fix: logo size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <Image src={"logo.svg"} alt="CareFence logo" width={360} height={100} />
-      <p className="italic mt-4 text-xl">&quot;Clock in within the safe zone&quot;</p>
+      <Image src={"logo.svg"} alt="CareFence logo" width={300} height={100} />
+      <p className="italic mt-4 text-lg">&quot;Clock in within the safe zone&quot;</p>
     </div>
   );
 }
